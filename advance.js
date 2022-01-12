83f08dbd17e564835472ea9f8afbc78dfbc952f8
Lemres <info@mxnuuel.de>
2022-01-12 10:35:50 +0100

    adding port for status page
    port forwarding

--- advance.js
+++ advance.js
@@ -2,6 +2,12 @@ const bot = require('./src/discord/bot.js');
 const cron = require('./src/cron.js');
 const database = require('./src/helper/dbHelper.js');
 
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.listen(port)
+
 console.log("Starting osu!advance - Version 1.0 (Beta)")
 database.connect()
 bot();
